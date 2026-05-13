56637bbfe3900084ae85cdf845e74bc535fdfd79
John <cooljohne@gmail.com>
2026-05-13 17:40:12 +0100

    authentication with Kinde added to the form

--- actions/submit.ts
+++ actions/submit.ts
@@ -1,10 +1,18 @@
 'use server'
 
 import prisma from "@/lib/prisma";
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { revalidatePath } from "next/cache";
+import { redirect } from "next/dist/client/components/navigation";
 
 
 export async function handleSubmit(formData: FormData) {
+    const {isAuthenticated} = getKindeServerSession()
+
+    if (!isAuthenticated) {
+        redirect('/api/auth/login')
+    }
+
     const title = formData.get('title') as string
     const content = formData.get('content') as string
     console.log({ title, content })
